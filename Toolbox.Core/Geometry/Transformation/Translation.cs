bd355dd10a3828db4a4bf2836990bf1264d423f2
fabien-vavrand <fabien.vavrand@gmail.com>
2016-03-24 15:07:20 +0100

    Update Translation.cs

--- Toolbox.Core/Geometry/Transformation/Translation.cs
+++ Toolbox.Core/Geometry/Transformation/Translation.cs
@@ -7,5 +7,21 @@ namespace Toolbox.Geometry.Transformation
 {
     public class Translation
     {
+        public Vector Vector { get; set; }
+
+        public Translation(Vector vector)
+        {
+            Vector = vector;
+        }
+
+        public Point Translate(Point point)
+        {
+            return Vector.Add(Vector, point);
+        }
+
+        public Rect Translate(Rect rect)
+        {
+            return new Rect(rect.Left + Vector.X, rect.Top + Vector.Y, rect.Width, rect.Height);
+        }
     }
 }
