08c128ec10b978835d2653f29d07668c2fcd8c4c
fabien-vavrand <fabien.vavrand@gmail.com>
2016-03-24 15:06:38 +0100

    Update Homothety.cs

--- Toolbox.Core/Geometry/Transformation/Homothety.cs
+++ Toolbox.Core/Geometry/Transformation/Homothety.cs
@@ -7,5 +7,26 @@ namespace Toolbox.Geometry.Transformation
 {
     public class Homothety
     {
+        public Point Center { get; set; }
+        public double Coefficient { get; set; }
+
+        public Homothety(Point center, double coefficient)
+        {
+            Center = center;
+            Coefficient = coefficient;
+        }
+
+        public Point Transform(Point point)
+        {
+            return new Point(
+                Center.X + Coefficient * (point.X - Center.X),
+                Center.Y + Coefficient * (point.Y - Center.Y));
+        }
+
+        public Rect Transform(Rect rect)
+        {
+            Point point = Transform(new Point(rect.X, rect.Y));
+            return new Rect(point.X, point.Y, rect.Width * Coefficient, rect.Height * Coefficient);
+        }
     }
 }
